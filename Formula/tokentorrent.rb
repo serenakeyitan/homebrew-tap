33ec9f0d7ff58518cc461ffdb86ef6da1e12353e
Serena <94026305+serenakeyitan@users.noreply.github.com>
2026-09-03 16:31:59 -0700

    Update TokenTorrent CLI to 0.1.28

--- Formula/tokentorrent.rb
+++ Formula/tokentorrent.rb
@@ -1,7 +1,7 @@
 class Tokentorrent < Formula
   desc "Connect a Mac to the TokenTorrent capacity marketplace"
-  url "https://registry.npmjs.org/tokentorrent/-/tokentorrent-0.1.21.tgz"
-  sha256 "0721781143518c410b87bc08fa4114f3ad626180e9328fc75590ad9f3bd09592"
+  url "https://registry.npmjs.org/tokentorrent/-/tokentorrent-0.1.28.tgz"
+  sha256 "8dbf167d206a6a9579fb8aec3d3775a31734e1df0c2d3525c23bf83525c3b067"
   license :cannot_represent
 
   depends_on "node@22"
